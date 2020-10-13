62a31de3f48d5fca134fc7a5038d7d04a21912ec
Guilherme <guiii@live.com>
2020-10-13 19:29:47 -0300

    Add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import './database/connection';
@@ -7,6 +8,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
