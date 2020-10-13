d2d1aff9557f3482e2ab40e9a365fa04bdb4f51f
Guilherme <guiii@live.com>
2020-10-13 18:49:38 -0300

    Add orphanages & images views

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import './database/connection';
+import path from 'path';
 import routes from './routes';
+import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);
